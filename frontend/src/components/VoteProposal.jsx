ee63ab0eb1f755151bf92208f4ad4409023823f4
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-14 13:04:42 +0100

    Create VoteProposal.jsx

---
+++ frontend/src/components/VoteProposal.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { governance } from "../utils/contractInteraction";
+
+export default function VoteProposal() {
+  const [proposalId, setProposalId] = useState("");
+  const [support, setSupport] = useState(true);
+
+  const vote = async () => {
+    await governance.vote(proposalId, support);
+    alert("Vote submitted!");
+  };
+
+  return (
+    <div>
+      <h3>Vote on Proposal</h3>
+      <input placeholder="Proposal ID" value={proposalId} onChange={(e) => setProposalId(e.target.value)} />
+      <select value={support} onChange={(e) => setSupport(e.target.value === "true")}>
+        <option value="true">Yes</option>
+        <option value="false">No</option>
+      </select>
+      <button onClick={vote}>Vote</button>
+    </div>
+  );
+}
