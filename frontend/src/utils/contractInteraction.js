8a61e4a143c8aaf166d69e170692e1fffcbebe1b
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-14 13:01:33 +0100

    Refactor contract interaction to use signer and new contracts

--- frontend/src/utils/contractInteraction.js
+++ frontend/src/utils/contractInteraction.js
@@ -1,12 +1,16 @@
 import { ethers } from "ethers";
-import contractAddresses from "../contracts-address.json";
 import DAOTokenABI from "../contracts/DAOToken.json";
 import GovernanceABI from "../contracts/Governance.json";
+import ProposalExecutorABI from "../contracts/ProposalExecutor.json";
+import TreasuryABI from "../contracts/Treasury.json";
+import addresses from "../contracts-address.json";
 
-export const getDAOTokenContract = (provider) => {
-  return new ethers.Contract(contractAddresses.DAOToken, DAOTokenABI, provider);
-};
+// Create provider and signer
+const provider = new ethers.BrowserProvider(window.ethereum);
+const signer = provider.getSigner();
 
-export const getGovernanceContract = (provider) => {
-  return new ethers.Contract(contractAddresses.Governance, GovernanceABI, provider);
-};
+// Contract instances
+export const daoToken = new ethers.Contract(addresses.DAOToken, DAOTokenABI, signer);
+export const governance = new ethers.Contract(addresses.Governance, GovernanceABI, signer);
+export const executor = new ethers.Contract(addresses.ProposalExecutor, ProposalExecutorABI, signer);
+export const treasury = new ethers.Contract(addresses.Treasury, TreasuryABI, signer);
