80fd03977f9300351c5ccf7c381a2c98c4ce2f2f
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-15 22:25:54 +0100

    deleted local files: hardhat.config.js package-lock.json

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,11 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
-  solidity: "0.8.20",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
-    }
-  }
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
