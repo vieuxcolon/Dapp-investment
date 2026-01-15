d1048ed846571b1aa0df172957de315495b1c987
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-15 13:39:05 +0100

    Update hardhat.config.js

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,16 +1,11 @@
-import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-dotenv.config();
-
-export default {
+module.exports = {
   solidity: "0.8.20",
   networks: {
     localhost: {
-      url: process.env.LOCAL_RPC_URL || "http://127.0.0.1:8545",
-      accounts: process.env.PRIVATE_KEY
-        ? [process.env.PRIVATE_KEY]
-        : []
+      url: "http://127.0.0.1:8545"
     }
   }
 };
