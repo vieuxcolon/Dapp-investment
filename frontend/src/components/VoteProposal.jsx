e53bce299a6fd796ad1c42707053f1def402de2e
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-15 08:31:14 +0100

    Update VoteProposal.jsx

--- frontend/src/components/VoteProposal.jsx
+++ frontend/src/components/VoteProposal.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
-import { governance } from "../utils/contractInteraction";
+import { getContracts } from "../utils/contractInteraction";
 
 export default function VoteProposal() {
   const [proposalId, setProposalId] = useState("");
   const [support, setSupport] = useState(true);
 
-  const vote = async () => {
-    await governance.vote(proposalId, support);
+  async function vote() {
+    const { governance } = await getContracts();
+    const tx = await governance.vote(proposalId, support);
+    await tx.wait();
     alert("Vote submitted!");
-  };
+  }
 
   return (
     <div>
       <h3>Vote on Proposal</h3>
-      <input placeholder="Proposal ID" value={proposalId} onChange={(e) => setProposalId(e.target.value)} />
+      <input value={proposalId} onChange={(e) => setProposalId(e.target.value)} placeholder="Proposal ID" />
       <select value={support} onChange={(e) => setSupport(e.target.value === "true")}>
         <option value="true">Yes</option>
         <option value="false">No</option>
@@ -22,4 +24,3 @@ export default function VoteProposal() {
     </div>
   );
 }
-
