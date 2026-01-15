eb99fdf461c4f5b19116da0004b048db958c0b0a
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-15 05:23:52 +0100

    Update contractInteraction.js

--- frontend/src/utils/contractInteraction.js
+++ frontend/src/utils/contractInteraction.js
@@ -3,14 +3,19 @@ import DAOTokenABI from "../contracts/DAOToken.json";
 import GovernanceABI from "../contracts/Governance.json";
 import ProposalExecutorABI from "../contracts/ProposalExecutor.json";
 import TreasuryABI from "../contracts/Treasury.json";
-import addresses from "../contracts-address.json";
+
+// Use environment variables instead of static addresses
+const DAOTokenAddress = process.env.REACT_APP_DAOTOKEN_ADDRESS;
+const GovernanceAddress = process.env.REACT_APP_GOVERNANCE_ADDRESS;
+const ProposalExecutorAddress = process.env.REACT_APP_PROPOSALEXECUTOR_ADDRESS;
+const TreasuryAddress = process.env.REACT_APP_TREASURY_ADDRESS;
 
 // Create provider and signer
 const provider = new ethers.BrowserProvider(window.ethereum);
 const signer = provider.getSigner();
 
 // Contract instances
-export const daoToken = new ethers.Contract(addresses.DAOToken, DAOTokenABI, signer);
-export const governance = new ethers.Contract(addresses.Governance, GovernanceABI, signer);
-export const executor = new ethers.Contract(addresses.ProposalExecutor, ProposalExecutorABI, signer);
-export const treasury = new ethers.Contract(addresses.Treasury, TreasuryABI, signer);
+export const daoToken = new ethers.Contract(DAOTokenAddress, DAOTokenABI, signer);
+export const governance = new ethers.Contract(GovernanceAddress, GovernanceABI, signer);
+export const executor = new ethers.Contract(ProposalExecutorAddress, ProposalExecutorABI, signer);
+export const treasury = new ethers.Contract(TreasuryAddress, TreasuryABI, signer);
